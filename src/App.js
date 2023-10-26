6d6a7a65ce623c8e5403bd042f97cff4fff2969e
OlgaMakeikina <makeikina2202@gmail.com>
2023-10-26 19:51:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+
+function App() { 
+  const [advice, setAdvice] = useState("");
+
+  const getAdvice = async () => {
+    const response = await fetch(`https://www.boredapi.com/api/activity/`);
+    const data = await response.json();
+    setAdvice(data.activity);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   <div className='container'>
+    <h1 >What should I do today?</h1>
+  
+
+   <div className='tip'>
+   <h2>{advice}</h2>
+   <button onClick={getAdvice}>New Advice</button>
+   </div>
+   </div>
     </div>
   );
 }
